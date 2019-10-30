2ffdf0624f5ec8dd279846a434fcad2727446c38
ofekgani <ag8339@bs.amalnet.k12.il>
2019-10-30 11:30:38 +0200

    idoefes

---
+++ app/src/main/java/com/example/spinner/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.spinner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+}
