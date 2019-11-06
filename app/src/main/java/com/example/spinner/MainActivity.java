29f15dfbc8523cb4445c300600f4fa2463b92b07
ofekgani <ag8339@bs.amalnet.k12.il>
2019-11-06 11:35:46 +0200

    2.0

--- app/src/main/java/com/example/spinner/MainActivity.java
+++ app/src/main/java/com/example/spinner/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.spinner;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        linearLayout = findViewById(R.id.layout);
     }
 
     public boolean onCreateOptionsMenu(Menu menu){
@@ -17,4 +24,21 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String st = item.getTitle().toString();
+        if(st.equals("White")){
+            linearLayout.setBackgroundColor(Color.WHITE);
+        }
+        if(st.equals("Green")){
+            linearLayout.setBackgroundColor(Color.GREEN);
+        }
+        if(st.equals("Red")){
+            linearLayout.setBackgroundColor(Color.RED);
+        }
+        if(st.equals("Blue")){
+            linearLayout.setBackgroundColor(Color.BLUE);
+        }
+        return true;
+    }
 }
